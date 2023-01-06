9773823a6168ed3e806735ea68e7a17890cc3ba2
satou <sato1212aaaaa@gmail.com>
2023-01-06 14:54:20 +0900

    test(CarTest): add car id

--- src/test/kotlin/domein/model/car/CarTest.kt
+++ src/test/kotlin/domein/model/car/CarTest.kt
@@ -10,13 +10,14 @@ private class CarTest {
 
     @Test
     fun `create method return instance as expect`() {
-        val tire = Tire.create("tire")
+        val carId = UUID.fromString("52adab23-ef19-49c7-aad8-46fa2ad6d84e")
+        val tire = Tire.create(carId,"tire")
         val actual = Car.create(
-            carId = UUID.fromString("52adab23-ef19-49c7-aad8-46fa2ad6d84e"),
+            carId = carId,
             tire = tire,
             )
         val expect = Car.create(
-            carId = UUID.fromString("52adab23-ef19-49c7-aad8-46fa2ad6d84e"),
+            carId = carId,
             tire = tire,
         )
 
