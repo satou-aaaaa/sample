40bb0795b759083f56cca72ca0aacf30b23ba0fa
satou <sato1212aaaaa@gmail.com>
2023-01-06 15:52:11 +0900

    refactor(Tire): remove car id

--- src/main/kotlin/domein/model/car/Tire.kt
+++ src/main/kotlin/domein/model/car/Tire.kt
@@ -3,16 +3,14 @@ package domein.model.car
 import java.util.UUID
 
 data class Tire(
-    val carId:UUID,
     val tireId:UUID,
     val value:String,
 ){
 
     companion object {
 
-        fun create(carId:UUID,value: String): Tire {
+        fun create(value: String): Tire {
             return Tire(
-                carId = carId,
                 tireId = UUID.randomUUID(),
                 value = value,
                 )
--- src/test/kotlin/domein/model/car/TireTest.kt
+++ src/test/kotlin/domein/model/car/TireTest.kt
@@ -9,11 +9,9 @@ private class TireTest {
 
     @Test
     fun `create method return instance as expect`() {
-        val carId = UUID.fromString("e23b42ae-4d56-4545-8c7d-857ec1dad0a1")
-        val actual = Tire.create(carId,"tire")
-        val expect = Tire.create(carId,"tire")
+        val actual = Tire.create("tire")
+        val expect = Tire.create("tire")
 
-        assertEquals(expect.carId,actual.carId)
         assertNotEquals(expect.tireId,actual.tireId)
         assertEquals(expect.value,actual.value)
     }
