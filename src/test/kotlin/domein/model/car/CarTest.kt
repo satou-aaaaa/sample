20cacc491bf6b07dfd9e0ccd19dcde136c669501
satou <sato1212aaaaa@gmail.com>
2023-01-06 14:28:13 +0900

    test (CarTest) : change to use car id

--- src/test/kotlin/domein/model/car/CarTest.kt
+++ src/test/kotlin/domein/model/car/CarTest.kt
@@ -2,6 +2,7 @@ package domein.model.car
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.util.*
 
 
 private class CarTest {
@@ -10,8 +11,14 @@ private class CarTest {
     @Test
     fun `create method return instance as expect`() {
         val tire = Tire("tire")
-        val actual = Car.create(tire)
-        val expect = Car.create(tire)
+        val actual = Car.create(
+            carId = UUID.fromString("52adab23-ef19-49c7-aad8-46fa2ad6d84e"),
+            tire = tire,
+            )
+        val expect = Car.create(
+            carId = UUID.fromString("52adab23-ef19-49c7-aad8-46fa2ad6d84e"),
+            tire = tire,
+        )
 
         assertEquals(expect.tire,actual.tire)
     }
