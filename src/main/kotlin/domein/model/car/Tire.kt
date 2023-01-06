d94640c714f3f987be96de4d9c61ed36e66052ea
satou <sato1212aaaaa@gmail.com>
2023-01-06 14:41:01 +0900

    feat(Tire): add create method

--- src/main/kotlin/domein/model/car/Tire.kt
+++ src/main/kotlin/domein/model/car/Tire.kt
@@ -1,3 +1,11 @@
 package domein.model.car
 
-data class Tire(val value:String)
+data class Tire(val value:String){
+
+    companion object {
+
+        fun create(value: String): Tire {
+            return Tire(value)
+        }
+    }
+}
