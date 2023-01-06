80ad2a36438d47d6417659e4c129bd7b184a4abb
satou <sato1212aaaaa@gmail.com>
2023-01-06 14:52:20 +0900

    feat(Tire): add car id

--- src/main/kotlin/domein/model/car/Tire.kt
+++ src/main/kotlin/domein/model/car/Tire.kt
@@ -3,14 +3,16 @@ package domein.model.car
 import java.util.UUID
 
 data class Tire(
+    val carId:UUID,
     val tireId:UUID,
     val value:String,
 ){
 
     companion object {
 
-        fun create(value: String): Tire {
+        fun create(carId:UUID,value: String): Tire {
             return Tire(
+                carId = carId,
                 tireId = UUID.randomUUID(),
                 value = value,
                 )
