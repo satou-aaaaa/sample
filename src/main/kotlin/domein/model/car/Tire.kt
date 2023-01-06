aba43613df76c9ccf363e1a79a3adcbadf847265
satou <sato1212aaaaa@gmail.com>
2023-01-06 14:43:50 +0900

    feat(tire): add tire id

--- src/main/kotlin/domein/model/car/Tire.kt
+++ src/main/kotlin/domein/model/car/Tire.kt
@@ -1,11 +1,19 @@
 package domein.model.car
 
-data class Tire(val value:String){
+import java.util.UUID
+
+data class Tire(
+    val tireId:UUID,
+    val value:String,
+){
 
     companion object {
 
         fun create(value: String): Tire {
-            return Tire(value)
+            return Tire(
+                tireId = UUID.randomUUID(),
+                value = value,
+                )
         }
     }
 }
