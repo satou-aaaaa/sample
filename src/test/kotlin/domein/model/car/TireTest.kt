9a864b61b6287cc62cd33f628b9bc48000772260
satou <sato1212aaaaa@gmail.com>
2023-01-06 14:45:01 +0900

    test(tiretest): change assert

--- src/test/kotlin/domein/model/car/TireTest.kt
+++ src/test/kotlin/domein/model/car/TireTest.kt
@@ -5,20 +5,13 @@ import org.junit.jupiter.api.Test
 
 private class TireTest {
 
-    @Test
-    fun test() {
-        val actual = Tire("tire")
-        val expect = Tire("tire")
-
-        assertEquals(expect,actual)
-    }
 
     @Test
     fun `create method return instance as expect`() {
         val actual = Tire.create("tire")
         val expect = Tire.create("tire")
 
-        assertEquals(expect.tireId,actual.tireId)
+        assertNotEquals(expect.tireId,actual.tireId)
         assertEquals(expect.value,actual.value)
     }
 }
