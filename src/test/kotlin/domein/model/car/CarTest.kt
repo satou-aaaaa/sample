c404eb190fe324ec261f97ac96b9d3c1976bae36
satou <sato1212aaaaa@gmail.com>
2023-01-06 14:45:15 +0900

    test(CarTest): change user create method

--- src/test/kotlin/domein/model/car/CarTest.kt
+++ src/test/kotlin/domein/model/car/CarTest.kt
@@ -10,7 +10,7 @@ private class CarTest {
 
     @Test
     fun `create method return instance as expect`() {
-        val tire = Tire("tire")
+        val tire = Tire.create("tire")
         val actual = Car.create(
             carId = UUID.fromString("52adab23-ef19-49c7-aad8-46fa2ad6d84e"),
             tire = tire,
